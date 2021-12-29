b24d9866737140647f907d7042757a945db46e37
Josh Roppo <joshroppo@gmail.com>
2021-12-29 15:33:03 -0800

    Tides graph working!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { greet } from "herron-island";
 
 const Http = new XMLHttpRequest();
 const Url = "https://tidesandcurrents.noaa.gov/cgi-bin/stationtideinfo.cgi?Stationid=9446583"
+/*
 Http.open("GET", Url);
 Http.send();
 
@@ -11,12 +12,13 @@ Http.onreadystatechange = (e) => {
     console.log(Http.responseText)
     greet(Http.responseText);
 }
+*/
 
 //wasm.greet();
 
 // If you only use `npm` you can simply
 // import { Chart } from "wasm-demo" and remove `setup` call from `bootstrap.js`.
-class Chart {}
+class Chart { }
 
 const canvas = document.getElementById("canvas");
 const coord = document.getElementById("coord");
@@ -30,12 +32,12 @@ let chart = null;
 
 /** Main entry point */
 export function main() {
-	let hash = location.hash.substr(1);
-	for(var i = 0; i < plotType.options.length; i++) {
-		if(hash == plotType.options[i].value) {
-			plotType.value = hash;
-		}
-	}
+    let hash = location.hash.substr(1);
+    for (var i = 0; i < plotType.options.length; i++) {
+        if (hash == plotType.options[i].value) {
+            plotType.value = hash;
+        }
+    }
     setupUI();
     setupCanvas();
 }
@@ -49,17 +51,17 @@ export function setup(WasmChart) {
 function setupUI() {
     status.innerText = "WebAssembly loaded!";
     plotType.addEventListener("change", updatePlot);
-	yaw.addEventListener("change", updatePlot);
-	pitch.addEventListener("change", updatePlot);
-	yaw.addEventListener("input", updatePlot);
-	pitch.addEventListener("input", updatePlot);
+    yaw.addEventListener("change", updatePlot);
+    pitch.addEventListener("change", updatePlot);
+    yaw.addEventListener("input", updatePlot);
+    pitch.addEventListener("input", updatePlot);
     window.addEventListener("resize", setupCanvas);
     window.addEventListener("mousemove", onMouseMove);
 }
 
 /** Setup canvas to properly handle high DPI and redraw current plot. */
 function setupCanvas() {
-	const dpr = window.devicePixelRatio || 1.0;
+    const dpr = window.devicePixelRatio || 1.0;
     const aspectRatio = canvas.width / canvas.height;
     const size = canvas.parentNode.offsetWidth * 0.8;
     canvas.style.width = size + "px";
@@ -72,26 +74,26 @@ function setupCanvas() {
 /** Update displayed coordinates. */
 function onMouseMove(event) {
     if (chart) {
-		var text = "Mouse pointer is out of range";
+        var text = "Mouse pointer is out of range";
 
-		if(event.target == canvas) {
-			let actualRect = canvas.getBoundingClientRect();
-			let logicX = event.offsetX * canvas.width / actualRect.width;
-			let logicY = event.offsetY * canvas.height / actualRect.height;
-			const point = chart.coord(logicX, logicY);
-			text = (point) 
-				? `(${point.x.toFixed(3)}, ${point.y.toFixed(3)})`
-				: text;
-		}
+        if (event.target == canvas) {
+            let actualRect = canvas.getBoundingClientRect();
+            let logicX = event.offsetX * canvas.width / actualRect.width;
+            let logicY = event.offsetY * canvas.height / actualRect.height;
+            const point = chart.coord(logicX, logicY);
+            text = (point)
+                ? `(${point.x.toFixed(3)}, ${point.y.toFixed(3)})`
+                : text;
+        }
         coord.innerText = text;
     }
 }
 
 function updatePlot3d() {
-	let yaw_value = Number(yaw.value) / 100.0;
-	let pitch_value = Number(pitch.value) / 100.0;
-	Chart.plot3d(canvas, pitch_value, yaw_value);
-	coord.innerText = `Pitch:${pitch_value}, Yaw:${yaw_value}`
+    let yaw_value = Number(yaw.value) / 100.0;
+    let pitch_value = Number(pitch.value) / 100.0;
+    Chart.plot3d(canvas, pitch_value, yaw_value);
+    coord.innerText = `Pitch:${pitch_value}, Yaw:${yaw_value}`
 }
 
 /** Redraw currently selected plot. */
@@ -100,20 +102,29 @@ function updatePlot() {
     status.innerText = `Rendering ${selected.innerText}...`;
     chart = null;
     const start = performance.now();
-	switch(selected.value) {
-		case "mandelbrot":
-			control.classList.add("hide");
-			chart = Chart.mandelbrot(canvas);
-			break;
-		case "3d-plot": 
-			control.classList.remove("hide");
-			updatePlot3d();
-			break;
-		default:
-			control.classList.add("hide");
-			chart = Chart.power("canvas", Number(selected.value))
-	}
-	
+    switch (selected.value) {
+        case "mandelbrot":
+            control.classList.add("hide");
+            chart = Chart.mandelbrot(canvas);
+            break;
+        case "3d-plot":
+            control.classList.remove("hide");
+            updatePlot3d();
+            break;
+        case "tides":
+            control.classList.add("hide");
+            Http.open("GET", Url);
+            Http.send();
+            Http.onreadystatechange = (e) => {
+                var x = Http.responseText;
+                console.log(Http.responseText)
+                chart = Chart.tides("canvas", Http.responseText)
+            }
+        default:
+            control.classList.add("hide");
+            chart = Chart.power("canvas", Number(selected.value))
+    }
+
     const end = performance.now();
     status.innerText = `Rendered ${selected.innerText} in ${Math.ceil(end - start)}ms`;
 }
